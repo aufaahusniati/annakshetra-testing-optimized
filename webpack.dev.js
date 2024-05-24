f71fe4f55d13086aa07aae594e9b83e6388e0d95
aufaahusniati <aufaahsnt21@gmail.com>
2024-05-24 22:32:30 +0700

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -16,4 +17,7 @@ module.exports = merge(common, {
       },
     },
   },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+  ]
 });
